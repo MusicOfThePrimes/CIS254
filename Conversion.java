49e4dd460847a1df7a090491a1f08f3b16e3e32b
Huilin Reid <huilin.reid@gmail.com>
2025-09-11 11:15:58 -0700

    modified comments

--- Conversion.java
+++ Conversion.java
@@ -2,11 +2,10 @@
 /**
  * Partner Lab 1
  * Description: This program is designed to convert between different 
- * currencies using predefined/quoted exchange rates. It supports 
- * conversions between:
- *     US dollars and Euros
- *                     
- * ....
+ * currencies using predefined/quoted exchange rates. 
+ * It supports currency conversions between:
+ *     - US dollars and Euros
+ *     - US dollars and Japanese Yen
  * 
  * @author Huilin Reid, Vixen La Ruk
  * @since September 10, 2025
@@ -19,8 +18,8 @@
  * e.g. Choose a conversion option:
  *      1. Euro -> US Dollar
  *      2. US Dollar -> Euro
- *      3. YEN -> US Dollar
- *      4. US Dollar -> Yen
+ *      3. Japanese Yen -> US Dollar
+ *      4. US Dollar -> Japanese Yen
  *      Your choice: 4
  *      Enter the amount to convert: 100
  *      $100.0 = ¥14735.00
@@ -28,8 +27,8 @@
  *      Choose a conversion option:
  *      1. Euro -> US Dollar
  *      2. US Dollar -> Euro
- *      3. YEN -> US Dollar
- *      4. US Dollar -> Yen
+ *      3. Japanese Yen -> US Dollar
+ *      4. US Dollar -> Japanese Yen
  *      Your choice: 5
  *      Invalid choice.
  */
@@ -39,49 +38,52 @@ import java.text.DecimalFormat;
 
 public class Conversion {
     // constants for conversion ratios
-    static final double DOLLAR_EURO_RATIO = 0.85; // quoted on Sep 9, 2025
-    static final double DOLLAR_YEN_RATIO = 147.35; // quoted on Sep 9, 2025
+    static final double DOLLAR_EURO_RATIO = 0.85; // US Dollar to Euro, quoted on Sep 9, 2025
+    static final double DOLLAR_YEN_RATIO = 147.35; // US dollar to Japanese Yen, quoted on Sep 9, 2025
 
-    /** This method converts dollar to euro, 
-        using the inputed dollar amount.
-        @param amountInDollar the amount of dollars to be converted
-    */
+    /** This method converts a given amount in UD Dollars to Euros. 
+     * 
+     *  @param amountInDollar the amount of US Dollars to be converted
+     *  @return the equivalent amount in Euros
+     */
     public static double dollarToEuro(double amountInDollar) {
         return amountInDollar * DOLLAR_EURO_RATIO;
     }
 
-    
-    /** This method converts euro to dollar, 
-        using the inputed euro amount.
-        @param amountInEuro the amount of euros to be converted
-    */
+    /** This method converts a given amount in Euros to UD Dollars. 
+     *
+     *  @param amountInEuro the amount of Euros to be converted
+     *  @return the equivalent amount in US Dollars
+     */
     public static double euroToDollar(double amountInEuro) {
         return amountInEuro / DOLLAR_EURO_RATIO;
     }
 
-    /** This method converts yen to dollar, 
-        using the inputed yen amount.
-        @param amountInYen the amount of yens to be converted
-    */
-    public static double yenToDollar(double amountInYen) {
-        return amountInYen / DOLLAR_YEN_RATIO;
-    }
-
-    /** This method converts dollar to yen, 
-        using the inputed dollar amount.
-        @param amountInDollar the amount of dollars to be converted
-    */
+    /** This method converts a given amount in US Dollars to Japanese Yen. 
+     * 
+     *  @param amountInDollar the amount of dollars to be converted
+     *  @return the equivalent amount in Japanese Yen
+     */
     public static double dollarToYen(double amountInDollar) {
         return amountInDollar * DOLLAR_YEN_RATIO;
     }
 
+    /** This method converts a given amount in Japanese Yen to US Dollars. 
+     * 
+     *  @param amountInYen the amount of Japanese Yen to be converted
+     *  @return the equivalent amount in US Dollars
+     */
+    public static double yenToDollar(double amountInYen) {
+        return amountInYen / DOLLAR_YEN_RATIO;
+    }
+ 
     // menu method - displays currency conversion choices for user to choose
     public static int menu (Scanner sc) {
         System.out.println("Choose a conversion option:");
         System.out.println("1. Euro -> US Dollar");
         System.out.println("2. US Dollar -> Euro");
-        System.out.println("3. YEN -> US Dollar");
-        System.out.println("4. US Dollar -> Yen");
+        System.out.println("3. Japanese Yen -> US Dollar");
+        System.out.println("4. US Dollar -> Japanese Yen");
         System.out.print("Your choice: ");
         int choice = sc.nextInt();
         return choice;
@@ -105,23 +107,26 @@ public class Conversion {
             switch (choice) {
                 case 1:
                     result = euroToDollar(amount);
-                    // System.out.println(amount + " Euros = " + df.format(result) + " US Dollars");
+                    // Euros -> US Dollars
                     System.out.println("\u20AC" + amount + " = " + "\u0024" + df.format(result));
                     break;
                 case 2:
                     result = dollarToEuro(amount);
+                    // US Dollar -> Euros
                     System.out.println("\u0024" + amount + " = " + "\u20AC" + df.format(result));
                     break;
                 case 3:
                     result = yenToDollar(amount);
+                    // Japanese Yen -> US Dollar
                     System.out.println("\u00A5" + amount + " = " + "\u0024" + df.format(result));
                     break;
                 case 4:
                     result = dollarToYen(amount);
+                    // US Dollar -> Japanese Yen
                     System.out.println("\u0024" + amount + " = " + "\u00A5" + df.format(result));
                     break;
             }
         }
-        in.close();
+        in.close(); 
     }   
 }
